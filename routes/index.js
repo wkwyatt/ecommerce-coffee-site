aefab7e260b40db07dab9dd957f196b88546e843
Robert Bunch <rob@digitalcrafts.com>
2015-12-15 16:41:55 -0500

    Update index,js and register jade file

--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,107 @@
 var express = require('express');
 var passport = require('passport');
+//Require our account.js file which resides in models one dir up
 var Account = require('../models/account');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	console.log(req.user);
-	res.render('index', { user : req.user });
+router.get('/', function (req, res) {
+    //res.send(req.session);
+    res.render('index', { username : req.session.username });
 });
 
-/////////////	REGISTER GET ///////////////////
+////////////////////////////////////////
+////////////////REGISTER////////////////
+////////////////////////////////////////
+
+// Get the register page
 router.get('/register', function(req, res) {
     res.render('register', { });
 });
 
-/////////////	REGISTER POST ///////////////////
-router.post('/register', function(req, res, next){
-	Account.register(new Account(
-		{username: req.body.username,}), 
-		req.body.password,
-		function(error, account){
-			if(error){
-				console.log(error);
-				return res.render('register');
-			}else{
-				passport.authenticate('local')(req, res, function(){
-					req.session.username = req.body.username;
-					res.redirect('/')
-				});
-			}
-		});
+//Post to the register page
+router.post('/register', function(req, res) {
+    //The mongo statement to insert the new vars into the db
+    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+        if (err) {
+            return res.render('register', { err : err });
+        }
+        passport.authenticate('local')(req, res, function () {
+            req.session.username = req.body.username;
+            res.render('index', { username : req.session.username });
+        });
+    });
 });
 
-/////////////	LOGIN GET ///////////////////
-router.get('/login', function(req, res, next){
-	res.render('login')
-})
+/* ---------------------------- */
+/* ----------Login----------- */
+/* ---------------------------- */
+//Get the login page
+router.get('/login', function(req, res) {
 
-/////////////	LOGIN POST ///////////////////
-router.post('/login', passport.authenticate('local'), function(req, res) {
-	req.session.username = req.body.username;
+    //the user is already logged in
+    if(req.session.username){
+        res.redirect('/choices');
+    }
+    //req.query.login pulls the query parameters right out of the http headers!
+    //They are here and failed a login
+    if (req.query.failedlogin){
+        res.render('login', { failed : "Your username or password is incorrect." });    
+    }
+    //They are here and aren't logged in
+    res.render('login', { user : req.user });
+}).post('/login', function(req, res, next) {
+
+    if(req.body.getStarted){
+        Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+            if (err) {
+                return res.render('register', { err : err });
+            }
+            if(!err)
+            passport.authenticate('local')(req, res, function () {
+                req.session.username = req.body.username;
+                res.render('choices', { username : req.session.username });
+            });
+        });        
+    }
+
+    if (!req.body.getStarted){
+      passport.authenticate('local', function(err, user, info) {
+        if (err) {
+          return next(err); // will generate a 500 error
+        }
+        // Generate a JSON response reflecting authentication status
+        if (! user) {
+          return res.redirect('/login?failedlogin=1');
+        }
+        if (user){
+            // Passport session setup.
+            passport.serializeUser(function(user, done) {
+              console.log("serializing " + user.username);
+              done(null, user);
+            });
+
+            passport.deserializeUser(function(obj, done) {
+              console.log("deserializing " + obj);
+              done(null, obj);
+            });        
+            req.session.username = user.username;
+        }
+
+        return res.redirect('/choices');
+      })(req, res, next);
+    }
+});
+
+/* ---------------------------- */
+/* ----------Logout----------- */
+/* ---------------------------- */
+router.get('/logout', function(req, res) {
+    req.logout();
+    req.session.destroy();
     res.redirect('/');
 });
 
 
-function isLoggedIn(req, res, next) {
-	if(req.user.authenticated) {
-		console.log('the user is logged in!!!!!!!!')
-	}
-	console.log('user is not logged in==========')
-}
 
 module.exports = router;
