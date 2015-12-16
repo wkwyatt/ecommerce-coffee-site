26d3e9f2741239351c745a771ddf23ac463268e3
wkwyatt <wkwyatt1@gmail.com>
2015-12-16 17:20:04 -0500

    add post of choices & delivery page

--- models/account.js
+++ models/account.js
@@ -2,9 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
+// Create the Schema for the users account 
 var Account = new Schema({
 	username: String,
-	password: String
+	password: String,
+	grind: String,
+	frequency: String,
+	pounds: Number
 });
 
 Account.plugin(passportLocalMongoose);
--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.post('/register', function(req, res) {
         }
         passport.authenticate('local')(req, res, function () {
             req.session.username = req.body.username;
-            res.render('index', { username : req.session.username });
+            res.render('choices', { username : req.session.username });
         });
     });
 });
@@ -50,7 +50,9 @@ router.get('/login', function(req, res) {
     }
     //They are here and aren't logged in
     res.render('login', { user : req.user });
-}).post('/login', function(req, res, next) {
+});
+
+router.post('/login', function(req, res, next) {
 
     if(req.body.getStarted){
         Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
@@ -97,11 +99,87 @@ router.get('/login', function(req, res) {
 /* ----------Logout----------- */
 /* ---------------------------- */
 router.get('/logout', function(req, res) {
-    req.logout();
     req.session.destroy();
     res.redirect('/');
 });
 
+router.get('/choices', function (req, res, next){
+    //Make sure the use is logged in
+    if(req.session.username) {
+        // They belong here proceed with the page
+        // Check and see if they have set preferences
+        Account.findOne({ username: req.session.username },
+            function (err, doc){
+                // Ternary statement 
+                // same as if / else statement evaluating 'doc.grind'
+                // if true then set the value of 'currGrind' to doc.grind
+                // otherwise set the value of 'currGrind' to undefined
+                var currGrind = doc.grind ? doc.grind : undefined;
+                var currFrequency = doc.frequency ? doc.frequency : undefined;
+                var currPounds = doc.pounds ? doc.pounds : undefined;
 
+                // Render the choices view
+                res.render('choices', { 
+                    username: req.session.username,
+                    grind: currGrind,
+                    frequency: currFrequency,
+                    pounds: currPounds,
+                    grindSelect: [
+                        { val:"extraCourse", text:"Extra Course" },
+                        { val:"mediumCourse", text:"Medium Course" },
+                        { val:"course", text:"Course" },
+                        { val:"fine", text:"Fine" },
+                        { val:"veryFine", text:"Very Fine"}
+                    ]
+                });
+            });
+
+    } else {
+        // They shouldnt be here redirect them
+        res.redirect('/');
+    }
+
+})
+
+router.post('/choices', function (req, res, next){
+    // is the person logged in
+    if(req.session.username){
+
+        var newGrind = req.body.grind;
+        var newFrequency = req.body.frequency;
+        var newPounds = req.body.quarterPounds;
+
+        console.log(newGrind);
+        Account.findOneAndUpdate(
+            { username: req.session.username },
+            { 
+                grind: newGrind,
+                frequency: newFrequency,
+                pounds: newPounds
+            }, 
+            { upsert: true },
+            function (err, acct){
+
+                console.log(acct);
+                console.log("======acct=====");
+                console.log(newGrind);
+                console.log("======grind=====");
+                console.log(err);
+                console.log("======err=====");
+                if(err){
+                    res.send("There was an error saving your preferences.  Please re-enter or send this error to your help team!");
+                } else {
+                    acct.save;
+                    res.redirect('delivery');
+                }
+            });
+    }
+});
+
+router.get('/delivery', function(req, res, next){
+
+    res.render("delivery");
+
+}); 
 
 module.exports = router;
