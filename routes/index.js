cf6a10ffd7842f2f08f3decb3c6cab86cc0c1b06
Robert Bunch <rob@digitalcrafts.com>
2015-12-17 16:02:08 -0500

    Payment and stripe integration in test key

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ var passport = require('passport');
 //Require our account.js file which resides in models one dir up
 var Account = require('../models/account');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+var vars = require('../config/vars.json');
+var stripe = require("stripe")(
+  "sk_test_2bbpfxzyNVOpMeqzxDIJ2jK8"
+);
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -97,11 +102,12 @@ router.get('/choices', function (req, res, next){
 		Account.findOne(
 			{ username: req.session.username },
 			function (err, doc){
-				var currGrind = doc.grind ? doc.grind : undefined
+				var currPounds = doc.pounds;
+				var curFreq = doc.frequency;
+				var currGrind = doc.grind
+				//Render the choices view
+				res.render('choices', {username: req.session.username, pounds: currPounds, grind: currGrind, frequency: curFreq});
 			});
-
-		//Render the choices view
-		res.render('choices');
 	}else{
 		res.redirect('/');
 	}
@@ -110,77 +116,132 @@ router.get('/choices', function (req, res, next){
 router.post('/choices', function (req, res, next){
 	//Is the dude at the keyboard logged in
 	if(req.session.username){
-		Account.findOne({username: req.session.username},
-		function (err, doc){
-
-			//when data exists, find it here.
-			var grind = doc.grind;
-			var frequency = doc.frequency;
-			var pounds = doc.quarterPounds;
-
+		//Yes
+		var query = {username: req.session.username};
+		var update = {grind: req.body.grind, pounds: req.body.quarterPounds, frequency: req.body.frequency};
+		var options = {upsert: true};
+		Account.findOneAndUpdate(query, update, options, function(err, account) {
+		  if (err) {
+		    console.log('There was an error saving your preferences. Please re-enter or send this error to our help team: ' + err );
+		  }else{
+		  	account.save;
+		  }
 		});
-
-		var newGrind = req.body.grind;
-		var newFrequency = req.body.frequency;
-		var newPounds = req.body.quarterPounds;
-
-		Account.findOneAndUpdate(
-			{ username: req.session.username },
-			{ grind: newGrind },
-			{ upsert: true },
-			function (err, account){
-				if (err) {
-					res.send('There was an error saving your preferences. Please re-enter or send this error to our help team: ' + err );
-				}else{
-					console.log("----------")
-					console.log(account)
-					console.log("----------")
-					account.save;
-				}
-			}
-		)
-
-		Account.findOneAndUpdate(
-			{ username: req.session.username },
-			{ frequency: newFrequency },
-			{ upsert: true },
-			function (err, account){
-				if (err) {
-					res.send('There was an error saving your preferences. Please re-enter or send this error to our help team: ' + err );
-				}else{
-					console.log("----------")
-					console.log(account)
-					console.log("----------")
-					account.save;
-				}
-			}
-		)
-
-		Account.findOneAndUpdate(
-			{ username: req.session.username },
-			{ pounds: newPounds },
-			{ upsert: true },
-			function (err, account){
-				if (err) {
-					res.send('There was an error saving your preferences. Please re-enter or send this error to our help team: ' + err );
-				}else{
-					console.log("----------")
-					console.log(account)
-					console.log("----------")
-					account.save;
-				}
-			}
-		)
-
 		res.redirect('/delivery');
-	}	
+	}else{
+		//req.session.username is not set. Goodbye.
+		res.redirect('/')
+	}
 });
 
 router.get('/delivery', function (req, res, next){
-	res.send("<h1>Welcome to the delivery page.</h1>")
+    //If the user is logged in...
+    if(req.session.username){
+        Account.findOne({ "username": req.session.username}, function (err, doc, next){
+            var fullName = doc.fullName ? doc.fullName : undefined;
+            var address1 = doc.address1 ? doc.address1 : undefined;
+            var address2 = doc.address2 ? doc.address2 : undefined;
+            var city = doc.city ? doc.city : undefined;
+            var state = doc.city ? doc.state : undefined;
+            var zip = doc.zip ? doc.zip : undefined;
+            var nextDelivery = doc.nextOrderDate ? doc.nextOrderDate : undefined
+            res.render( 'delivery', {
+                username: req.session.username,
+                fullName: fullName,
+                address1: address1,
+                address2: address2,
+                city: city,
+                state: state,
+                zip: zip,
+                nextDelivery: nextDelivery
+            });
+        });
+    }    
+    if(!req.session.username){
+        //The user is not logged in. Send them to the login page.
+        res.redirect('/login');
+    }
+
+});
+
+router.get('/payment', function (req, res, next){
+    //If the user is logged in...
+    if(req.session.username){
+        Account.findOne({ "username": req.session.username}, function (err, doc, next){
+            var fullName = doc.fullName ? doc.fullName : undefined;
+            var address1 = doc.address1 ? doc.address1 : undefined;
+            var address2 = doc.address2 ? doc.address2 : undefined;
+            var city = doc.city ? doc.city : undefined;
+            var state = doc.city ? doc.state : undefined;
+            var zip = doc.zip ? doc.zip : undefined;
+            var nextDelivery = doc.nextOrderDate ? doc.nextOrderDate : undefined
+            res.render( 'payment', {
+                username: req.session.username,
+                fullName: fullName,
+                address1: address1,
+                address2: address2,
+                city: city,
+                state: state,
+                zip: zip,
+                nextDelivery: nextDelivery
+            });
+        });
+    }    
+    if(!req.session.username){
+        //The user is not logged in. Send them to the login page.
+        res.redirect('/login');
+    }    
+});
+
+router.get('/email', function (req, res, next){
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: vars.email,
+			pass: vars.password
+		}
+	});
+	var text = "This is a test email sent from my node server";
+	var mailOptions = {
+		from: body.req.name + '<' + body.req.email + '>',
+		to: 'Robert Bunch <rdbunch@gmail.com>',
+		subject: 'This is a test subject',
+		text: req.body.message + ' this email is from: '
+	}
+
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			res.json({response: error});
+		}else{
+			console.log("Message was successfully sent. Response was " + info.response);
+			res.json({response: "success"});
+		}
+	})
+
+});
+
+router.post('/payment', function(req, res, next){
+		stripe.charges.create({
+		  amount: 400,
+		  currency: "usd",
+		  source: req.body.stripeToken, // obtained with Stripe.js
+		  description: "Charge for " + req.body.stripeEmail
+		}, function(err, charge) {
+		  // asynchronously called
+		  console.log(charge)
+		  if(err){
+		  	res.send('you got an error.' + err)
+		  }else{
+		  	res.redirect('/thankyou')
+		  }
+		});
 });
 
 
+router.get('/contact', function(req, res, next){
+	res.render('contact');
+});
 
 module.exports = router;
 
